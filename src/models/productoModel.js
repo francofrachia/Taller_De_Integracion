13153c5ec291430b222bad70c73c1c04412f6fac
AgusB32 <fr.abenedetti1@gmail.com>
2026-04-07 13:17:16 -0300

    Config productoModel para manejo de imagenes

--- src/models/productoModel.js
+++ src/models/productoModel.js
@@ -1,16 +1,26 @@
 const pool = require('../config/db');
 
 const Producto = {
-    // Traer todos los productos con stock para el catálogo (CU03)
+    // Traer todos los productos con stock para el catálogo (CU03) + su imagen
     getAll: async () => {
-        const query = 'SELECT * FROM "producto" WHERE stock > 0';
+        const query = `
+            SELECT p.*, i.url AS imagen_url 
+            FROM producto p
+            LEFT JOIN imagen i ON p.id_producto = i.id_producto
+            WHERE p.stock > 0
+        `;
         const { rows } = await pool.query(query);
         return rows;
     },
 
-    // Traer detalle de un producto específico (CU04)
+    // Traer detalle de un producto específico (CU04) + su imagen
     getById: async (id) => {
-        const query = 'SELECT * FROM "producto" WHERE "idProducto" = $1';
+        const query = `
+            SELECT p.*, i.url AS imagen_url 
+            FROM producto p
+            LEFT JOIN imagen i ON p.id_producto = i.id_producto
+            WHERE p.id_producto = $1
+        `;
         const { rows } = await pool.query(query, [id]);
         return rows[0];
     }
